b53c4fd4c1a3a746cdd868034a9080f41b3db86e
Vivid-Vortex <ctntc9.dpk@gmail.com>
2024-04-10 10:30:00 +0530

    upto azure functions project

---
+++ AzureFunctionEcomDemo/AzureFunctionEcomDemo/src/main/java/com/ecomdemo/azurefunctions/AzureFunctionEcomDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ecomdemo.azurefunctions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureFunctionEcomDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureFunctionEcomDemoApplication.class, args);
+	}
+
+}
---
+++ AzureFunctionEcomDemo/AzureFunctionEcomDemo/src/test/java/com/ecomdemo/azurefunctions/AzureFunctionEcomDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomdemo.azurefunctions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureFunctionEcomDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
